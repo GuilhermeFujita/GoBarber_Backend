7e5eefb8a87ad0e44ac2ead879adbf687a7e0573
GuilhermeFujita <fujitaguilherme@gmail.com>
2020-08-09 23:46:41 -0300

    Dependency Injection

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,6 +1,8 @@
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import authConfig from '@config/auth';
+import { inject, injectable } from 'tsyringe';
+
 import AppError from '@shared/errors/AppError';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
@@ -15,8 +17,12 @@ interface IResponse {
     token: string;
 }
 
+@injectable()
 class AuthenticateUserService {
-    constructor(private usersRepository: IUsersRepository) {}
+    constructor(
+        @inject('UsersRepository')
+        private usersRepository: IUsersRepository,
+    ) {}
 
     public async execute({ email, password }: IRequest): Promise<IResponse> {
         const user = await this.usersRepository.findByEmail(email);
--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,18 +1,23 @@
 import { hash } from 'bcryptjs';
+import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 import User from '@modules/users/infra/typeorm/entities/User';
+import UsersRepository from '../infra/typeorm/repositories/UsersRepository';
 
 interface IRequest {
     name: string;
     email: string;
     password: string;
 }
-
+@injectable()
 class CreateUserService {
-    constructor(private usersRepository: IUsersRepository) {}
+    constructor(
+        @inject(UsersRepository)
+        private usersRepository: IUsersRepository,
+    ) {}
 
     public async execute({ name, email, password }: IRequest): Promise<User> {
         const checkUserExists = await this.usersRepository.findByEmail(email);
