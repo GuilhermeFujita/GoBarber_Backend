4d8b396ea711bfd5229b712ddeb1382717043837
GuilhermeFujita <fujitaguilherme@gmail.com>
2020-12-29 11:45:29 -0300

    User Avatar tests

--- src/shared/container/providers/StorageProvider/fakes/FakeStorageProvider.ts
+++ src/shared/container/providers/StorageProvider/fakes/FakeStorageProvider.ts
@@ -5,6 +5,7 @@ private storage: string[] = [];
 
   public async saveFile(file: string): Promise<string> {
     this.storage.push(file);
+    return file;
   }
 
   public async deleteFile(file: string): Promise<void> {
