9f8cbe6725dda36661af6126d486a463be524234
GuilhermeFujita <fujitaguilherme@gmail.com>
2020-12-30 11:27:04 -0300

    Password reset

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -19,5 +19,13 @@ export default class FakeUserTokensRepository implements IUserTokensRepository {
     this.userTokens.push(userToken);
 
     return userToken;
-  }   
+  }
+  
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      findToken => findToken.token == token
+    );
+
+    return userToken;
+  }
 }
