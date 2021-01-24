5a2d850335fa25397803d363d97490d4b22d09ad
GuilhermeFujita <fujitaguilherme@gmail.com>
2021-01-24 14:58:13 -0300

    Class Transformer

--- src/modules/users/infra/http/controllers/SessionsController.ts
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import { fromString } from 'uuidv4';
 
 export default class SessionsController{
   public async create(request: Request, response: Response): Promise<Response>{
@@ -13,9 +15,7 @@ export default class SessionsController{
         email,
         password,
     });
-
-    delete user.password;
-
-    return response.json({ user, token });
+    
+    return response.json({ user: classToClass(user), token });
   }
 }
