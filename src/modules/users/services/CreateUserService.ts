0929085a57089ee39b2b46aaecd252a26f31cf57
GuilhermeFujita <fujitaguilherme@gmail.com>
2021-01-25 16:53:25 -0300

    Cache invalidation

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -2,43 +2,49 @@ import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import User from '@modules/users/infra/typeorm/entities/User';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
-import User from '@modules/users/infra/typeorm/entities/User';
 import UsersRepository from '../infra/typeorm/repositories/UsersRepository';
 
 interface IRequest {
-    name: string;
-    email: string;
-    password: string;
+  name: string;
+  email: string;
+  password: string;
 }
 @injectable()
 class CreateUserService {
-    constructor(
-        @inject(UsersRepository)
-        private usersRepository: IUsersRepository,
+  constructor(
+    @inject(UsersRepository)
+    private usersRepository: IUsersRepository,
 
-        @inject('HashProvider')
-        private hashProvider: IHashProvider,
-    ) {}
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
 
-    public async execute({ name, email, password }: IRequest): Promise<User> {
-        const checkUserExists = await this.usersRepository.findByEmail(email);
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
+  ) { }
 
-        if (checkUserExists) {
-            throw new AppError('Email address already used');
-        }
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const checkUserExists = await this.usersRepository.findByEmail(email);
 
-        const hashesPassword = await this.hashProvider.generateHash(password);
-
-        const user = await this.usersRepository.create({
-            name,
-            email,
-            password: hashesPassword,
-        });
-
-        return user;
+    if (checkUserExists) {
+      throw new AppError('Email address already used');
     }
+
+    const hashesPassword = await this.hashProvider.generateHash(password);
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password: hashesPassword,
+    });
+
+    await this.cacheProvider.invalidatePrefix('providers-list');
+
+    return user;
+  }
 }
 
 export default CreateUserService;
