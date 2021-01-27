ba60cbbcb798c84277e5d25f697206347d1bf831
GuilhermeFujita <fujitaguilherme@gmail.com>
2021-01-26 23:34:02 -0300

    Appointments Cache

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -27,18 +27,24 @@ export default class ListProviderAppointmentsService {
     month,
     year,
   }: IRequest): Promise<Appointment[]> {
-    const cacheData = this.cacheProvider.recover('asd');
+    const cacheKey = `provider-appointments:${provider_id}-${year}-${month}-${day}`;
 
-    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
-      {
-        provider_id,
-        day,
-        month,
-        year,
-      },
+    let appointments = await this.cacheProvider.recover<Appointment[]>(
+      cacheKey,
     );
 
-    await this.cacheProvider.save('asd', 'asd');
+    if (!appointments) {
+      appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+        {
+          provider_id,
+          day,
+          month,
+          year,
+        },
+      );
+
+      await this.cacheProvider.save(cacheKey, appointments);
+    }
 
     return appointments;
   }
