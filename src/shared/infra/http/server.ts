3d4f1d7a4d64bf74bef9966adabc193a71f519a4
GuilhermeFujita <fujitaguilherme@gmail.com>
2021-01-29 23:48:44 -0300

    Express rate limiter

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -7,6 +7,7 @@ import { errors } from 'celebrate';
 import 'express-async-errors';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import rateLimiter from './middlewares/rateLimiter';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
@@ -15,6 +16,7 @@ import routes from './routes';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
@@ -24,23 +26,23 @@ app.use(routes);
 app.use(errors());
 
 app.use(
-    (err: Error, request: Request, response: Response, next: NextFunction) => {
-        if (err instanceof AppError) {
-            return response.status(err.statusCode).json({
-                status: 'error',
-                message: err.message,
-            });
-        }
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
 
-        console.error(err.message);
+    console.error(err.message);
 
-        return response.status(500).json({
-            status: 'error',
-            message: 'Internal server error',
-        });
-    },
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
 );
 
 app.listen(3333, () => {
-    console.log('Server started on port 3333');
+  console.log('Server started on port 3333');
 });
