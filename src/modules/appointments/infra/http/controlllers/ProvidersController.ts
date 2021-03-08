c99197886edb92e4aa0c652691bc6a14e4fee175
GuilhermeFujita <fujitaguilherme@gmail.com>
2021-03-08 13:57:19 -0300

    API fixes

--- src/modules/appointments/infra/http/controlllers/ProvidersController.ts
+++ src/modules/appointments/infra/http/controlllers/ProvidersController.ts
@@ -1,19 +1,19 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ListProvidersService from '@modules/appointments/services/ListProvidersService';
 
-export default class ProvidersController{
-  public async index(request: Request, response: Response): Promise<Response>{
-
+export default class ProvidersController {
+  public async index(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
 
     const listProviders = container.resolve(ListProvidersService);
 
     const providers = await listProviders.execute({
-      user_id
+      user_id,
     });
 
-    return response.json(providers);
+    return response.json(classToClass(providers));
   }
 }
